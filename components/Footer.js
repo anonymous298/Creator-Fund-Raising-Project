ab3b52645bf38039bc07a16342cf9bc638b53cdb
anonymous298 <tackletalha@gmail.com>
2025-08-30 01:27:14 +0500

    footer updated

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,8 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div className='bg-gray-800 p-6 flex justify-center items-center'>
-      <h2 className='text-white text-[20px] font-bold'>All Rights Reserved <span className='text-purple-600 text-[21px] font-extrabold cursor-pointer'>Patreon</span> &copy; | Made With ❤️ By Talha</h2>
+    <div className='bg-gray-900 p-6 flex justify-center items-center'>
+      <h2 className='text-white text-[20px] font-bold max-[400px]:text-[15px]'><span className='text-purple-600 text-[21px] font-extrabold cursor-pointer max-[400px]:text-[17px]'>Patreon</span> - All Rights Reserved &copy; | Made With ❤️ By Talha</h2>
     </div>
   )
 }
