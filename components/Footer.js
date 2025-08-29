0783c2473a791da3f701126f13a1a253cb95acee
anonymous298 <tackletalha@gmail.com>
2025-08-29 18:35:13 +0500

    footer component initialized

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Footer = () => {
+  return (
+    <div className='bg-gray-800 p-6 flex justify-center items-center'>
+      <h2 className='text-white text-[20px] font-bold'>All Rights Reserved <span className='text-purple-600 text-[21px] font-extrabold cursor-pointer'>Patreon</span> &copy; | Made With ❤️ By Talha</h2>
+    </div>
+  )
+}
+
+export default Footer
