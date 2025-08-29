3bcdbdff360190e0feb31402edb9591a0675d7d5
anonymous298 <tackletalha@gmail.com>
2025-08-30 01:28:16 +0500

    layout page updated

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import SessionWrapper from "@/components/SessionWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,16 +22,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      <SessionWrapper>
 
-      > 
-        <Navbar />
-          <div className=" min-h-screen">
-            {children}
-          </div>
-        <Footer />
-      </body>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          
+          > 
+        
+          <Navbar />
+            <div className=" min-h-screen text-white bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
+              {children}
+            </div>
+          <Footer />
+        </body>
+
+      </SessionWrapper>
     </html>
   );
 }
