301eea571fb69818c01c96ec7c51c026d471920a
anonymous298 <tackletalha@gmail.com>
2025-08-30 21:07:26 +0500

    server actions created

---
+++ actions/useractions.js
@@ -0,0 +1,35 @@
+"use server"
+import connectDb from "@/db/connectDB";
+import User from "@/models/User";
+
+export const fetchUser = async (username) => {
+    await connectDb()
+    const u = await User.findOne({username : username})
+    const user = u.toObject({flattenObjectIds : true})
+    return user
+    // return 'hello'
+}
+
+export const updateProfile = async (data, oldUsername) => {
+    await connectDb()
+    let newData = Object.fromEntries(data)
+    // console.log(newData)
+    // let user = await User.findOne({email : data.email})
+    if (oldUsername !== newData.username) {
+        let user = await User.findOne({username : newData.username});
+        // console.log(user)
+        
+        if (user) {
+            return "This user already exists try another username"
+        }
+
+        await User.updateOne({email : newData.email}, newData)
+        // alert('Updated')
+        console.log('updated')
+    }
+        
+    await User.updateOne({email : newData.email}, newData)
+    // alert('Updated')
+    console.log('updated')
+
+}
