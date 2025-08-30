73153c12645678467949d0e3ee02862daf2c1dcd
anonymous298 <tackletalha@gmail.com>
2025-08-30 23:31:15 +0500

    conected to mongodb atlas

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 
 const connectDb = async () => {
-        try {
-            const conn = await mongoose.connect('mongodb://localhost:27017/users', {
-                useNewUrlParser: true,
-            });
-            console.log(`MongoDB Connected: ${conn.connection.host}`);
-            return conn;
-            
-        } catch (error) {
-            console.error(error.message);
-            process.exit(1);
-        }
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true, // add this for Atlas
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    return conn;
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 
-  export default connectDb;
+export default connectDb;
